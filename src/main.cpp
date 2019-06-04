67a627ac88f596cbc82af0b7e263369b6665ade5
dengrenjie <dengrenjie31@gmail.com>
2019-06-04 13:36:38 +0800

    Delete rotation

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 
-GLfloat rotate_y = 0;
-GLfloat rotate_x = 0;
+// GLfloat rotate_y = 0;
+// GLfloat rotate_x = 0;
 
 void draw() {
 	glPushMatrix();
-	glTranslatef(0.0, -0.5, 0.0);
+	glTranslatef(0.0, -0.3, 0.0);
 	plate();
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(0.3, -0.5, 0.4);
+	glTranslatef(0.3, -0.3, 0.4);
 	glScalef(0.8, 0.8, 0.8);
 	cola();
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(0.4, -0.25, -0.3);
+	glTranslatef(0.4, -0.05, -0.3);
 	glScalef(0.6, 0.6, 0.6);
 	burger();
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(-0.3, -0.38, -0.1);
+	glTranslatef(-0.3, -0.18, -0.1);
 	glRotatef(120, 0.0, 1.0, 0.0);
 	glScalef(0.5, 0.5, 0.5);
 	chips();
@@ -36,11 +36,18 @@ void display() {
 	// Clear screen and buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glViewport(0, 0, 500, 500);
+	glMatrixMode(GL_PROJECTION);
 	// Reset transformations
 	glLoadIdentity();
+	gluPerspective(28.0, 1, 0.1, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(2.5, 2, -2.5, 0, 0, 0, 0, 1, 0);
+	draw();
 
-	glRotatef(rotate_x, 1.0, 0.0, 0.0);
-	glRotatef(rotate_y, 0.0, 1.0, 0.0);
+	// glRotatef(rotate_x, 1.0, 0.0, 0.0);
+	// glRotatef(rotate_y, 0.0, 1.0, 0.0);
 
 	// Main function of drawings
 	draw();
@@ -52,22 +59,22 @@ void display() {
 
 
 // specialKeys() Callback Function
-void specialKeys(int key, int x, int y) {
-	if (key == GLUT_KEY_RIGHT)
-		rotate_y += 5;
+// void specialKeys(int key, int x, int y) {
+// 	if (key == GLUT_KEY_RIGHT)
+// 		rotate_y += 5;
 
-	else if (key == GLUT_KEY_LEFT)
-		rotate_y -= 5;
+// 	else if (key == GLUT_KEY_LEFT)
+// 		rotate_y -= 5;
 
-	else if (key == GLUT_KEY_UP)
-		rotate_x += 5;
+// 	else if (key == GLUT_KEY_UP)
+// 		rotate_x += 5;
 
-	else if (key == GLUT_KEY_DOWN)
-		rotate_x -= 5;
+// 	else if (key == GLUT_KEY_DOWN)
+// 		rotate_x -= 5;
 
-	// Request display update
-	glutPostRedisplay();
-}
+// 	// Request display update
+// 	glutPostRedisplay();
+// }
 
 
 int main(int argc, char* argv[]) {
@@ -86,7 +93,7 @@ int main(int argc, char* argv[]) {
 
 	// Callback Functions
 	glutDisplayFunc(display);
-	glutSpecialFunc(specialKeys);
+	// glutSpecialFunc(specialKeys);
 
 	// Pass control to GLUT for events
 	glutMainLoop();
